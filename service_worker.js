652b0efe3e1e800cd8902e4fcccf49aed4d667e4
aball0386 <aaronball8145@gmail.com>
2025-06-15 17:59:06 +0100

    Add files via upload

---
+++ service_worker.js
@@ -0,0 +1,33 @@
+const CACHE_NAME = 'cfr-toolbox-cache-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icons/nhs.png',
+  './icons/secamb.png',
+  './icons/cfr_toolbox_icon_192.png',
+  './icons/cfr_toolbox_icon_512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+      )
+    )
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
+
